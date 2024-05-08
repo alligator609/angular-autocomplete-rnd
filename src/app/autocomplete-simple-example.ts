410ae52280344ae65b747e752d7c948d65341b92
Zia Uddin Ahmed <zia.ete10@gmail.com>
2024-05-08 21:55:50 +0600

    UPDATE: fixes filter issue

--- src/app/autocomplete-simple-example.ts
+++ src/app/autocomplete-simple-example.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 /**
  * @title Simple autocomplete
@@ -8,11 +10,11 @@ import { FormControl } from '@angular/forms';
   selector: 'autocomplete-simple-example',
   templateUrl: 'autocomplete-simple-example.html',
   styleUrls: ['autocomplete-simple-example.css'],
-})
-export class AutocompleteSimpleExample {
+})export class AutocompleteSimpleExample implements OnInit{
   myControl = new FormControl('1');
   options: string[] = ['One', 'Two', 'Three'];
-  books: Array<{ id: string; title: string }> = [
+  books$: Observable<Book[]>;
+  books: Array<Book> = [
     { id: '1', title: 'USD' },
     { id: '2', title: 'AUD' },
     { id: '3', title: 'INR' },
@@ -20,15 +22,35 @@ export class AutocompleteSimpleExample {
     { id: '5', title: 'GBP' },
   ];
 
+  ngOnInit() {
+    this.books$ = this.myControl.valueChanges.pipe(
+      startWith(""),
+      map((airport) =>
+        airport ? this.filterbooks(airport) : this.books.slice()
+      )
+    );
+  }
   getTitle(bookId: string) {
-    return this.books.find((book) => book.id === bookId).title;
+    return this.books.find((book) => book.id === bookId)?.title;
   }
 
   onSubmit() {
     console.log(this.myControl.value);
   }
+
+  filterbooks(name: string) {
+    return this.books.filter(
+      (airport) =>
+        airport.id.toLowerCase().indexOf(name.toLowerCase()) === 0 ||
+        airport.title.toLowerCase().indexOf(name.toLowerCase()) === 0
+    );
+  }
 }
 
+export class Book {
+  id: string;
+  title: string;
+}
 /**  Copyright 2019 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
     can be found in the LICENSE file at http://angular.io/license */
